fabcb772d86625638415d6493c215c403de9ecc0
wadoudadam <128430142+wadoudadam@users.noreply.github.com>
2025-07-30 18:54:01 +0100

    Openclassroom_main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,5 @@
-# Écrivez votre code ici !
+nom = Wadoud ADAM
+age = 25
+taille= 1.72
+est_etudiant = 0
+print(f"Bonjour, il s'appelle {nom} agé de {age} ans. Taille moyenne de {taille} et Etudiant {est_etudiant}.")
