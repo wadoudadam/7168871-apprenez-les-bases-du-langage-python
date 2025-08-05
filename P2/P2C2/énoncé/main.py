28f8a5eb53bf6ba1971e44c9f1a609c51ab08781
wadoudadam <128430142+wadoudadam@users.noreply.github.com>
2025-08-05 11:20:40 +0100

    Update main.py

--- P2/P2C2/énoncé/main.py
+++ P2/P2C2/énoncé/main.py
@@ -1,1 +1,17 @@
-# Ecrivez votre code ici !
+nombre=input("Entrez une liste de nombres séparés par des virgules: ")
+liste=nombre.split(",")
+l=[]
+for i in liste:
+    i=int(i)
+    l.append(i)
+
+s=0
+for i in l:
+
+    s+=i
+m=round(s/len(l),3)
+print(f"la somme des nombres {l} est {s}, et leur moyenne est {m}")
+
+for i in l:
+    if i>m:
+        print(i,"\t")# Ecrivez votre code ici !
