9c0183fd2df249f5e61f5deca5d063028cd2460a
wadoudadam <128430142+wadoudadam@users.noreply.github.com>
2025-08-10 19:59:38 +0100

    Update main.py

--- P3/P3C1/énoncé/main.py
+++ P3/P3C1/énoncé/main.py
@@ -1,1 +1,8 @@
 # Ecrivez votre code ici !
+import operations
+
+print("ee")
+if __name__=='__main__':
+    resultat1= operations.addition(3,5)
+    resultat2= operations.multiplication(3,5)
+    print("Addition & Produit :",resultat1,resultat2)
